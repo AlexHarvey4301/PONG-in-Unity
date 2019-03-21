e6ec09cb6185e37eacd3170572b0224dc9f38cc6
Alexander Harvey <alex.harvey.4301@gmail.com>
2019-03-21 13:21:59 -0700

    Import PONG files

---
+++ Assets/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Vector2 Pos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = Pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = gameObject.transform.position;
+        gameObject.transform.position = pos + new Vector2(0, -0.2f);
+    }
+}
